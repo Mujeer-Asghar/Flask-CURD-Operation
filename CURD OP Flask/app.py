37386a8cb3e31e4f456131315b4d67c5302ca77e
Metacore-Blockchain <mujeeerasghar7700@gmail.com>
2025-09-26 04:18:43 +0500

    Update project with latest changes

--- CURD OP Flask/app.py
+++ CURD OP Flask/app.py
@@ -1,9 +1,14 @@
 from flask import Flask,request,render_template,redirect
-
-# from models import db,StudentModel
-
+import pymysql
 
 app=Flask(__name__)
+
+# con=pymysql.connect(
+#     host='localhost',
+#     passwd='root',
+#     user='root',
+   
+# )
 
 # app.config['SQLAlCHEMY_DATABSE_URI']= 'sqlite:///students.db'
 # app.configx['SQLALCHEMY_TRACK_MODIFICATION']=False
@@ -13,11 +18,30 @@ app=Flask(__name__)
 # def create_Table():
 #     db.creat_all()
 
-
+data={}
 @app.route("/insertdata",methods=['GET',"POST"])
 def create():
+   if request.method=='POST':
+       email=request.form.get("email")
+       password=request.form.get("password")
+       data['email']=email
+       data['passwod']=password
+       file =open("templates/data.txt",'r')
+       stre=email+" "+password
+       with open('templates/data.txt','a') as file:
+        file.write(stre+"\n")
+       return redirect('/insert')
    
-        return render_template("create.html")
+   return render_template("sign.html")
+    
+       
+   
+@app.route('/insert')
+def show():
+
+    return render_template('sign.html',ali=data)
+
+      
         
         
 
