a7bfcaf2b5308f7f73231e39a4a5ee605a20c67a
Metacore-Blockchain <mujeeerasghar7700@gmail.com>
2025-09-30 03:23:29 +0500

    Added navbar with Add User, Update, and Delete buttons

--- CURD OP Flask/app.py
+++ CURD OP Flask/app.py
@@ -1,53 +1,57 @@
 from flask import Flask,request,render_template,redirect
-import pymysql
 
 app=Flask(__name__)
 
-# con=pymysql.connect(
-#     host='localhost',
-#     passwd='root',
-#     user='root',
-   
-# )
-
-# app.config['SQLAlCHEMY_DATABSE_URI']= 'sqlite:///students.db'
-# app.configx['SQLALCHEMY_TRACK_MODIFICATION']=False
-# db.int_app(app)
-
-# @app.before_first_request
-# def create_Table():
-#     db.creat_all()
-
-data={}
-@app.route("/insertdata",methods=['GET',"POST"])
-def create():
-   if request.method=='POST':
-       email=request.form.get("email")
-       password=request.form.get("password")
-       data['email']=email
-       data['passwod']=password
-       file =open("templates/data.txt",'r')
-       stre=email+" "+password
-       with open('templates/data.txt','a') as file:
-        file.write(stre+"\n")
-       return redirect('/insert')
-   
-   return render_template("sign.html")
-    
-       
-   
-@app.route('/insert')
-def show():
-
-    return render_template('sign.html',ali=data)
-
-      
-        
-        
 
 @app.route("/")
 def hello():
     return render_template("create.html")
+
+
+
+
+@app.route('/insert')
+def show():
+    return render_template('sign.html')
+
+
+@app.route("/insertdata",methods=['GET',"POST"])
+def create():
+   if request.method=='POST':
+       email=request.form.get("email")
+       password=request.form.get("password")       
+       stre=email+" "+password
+       with open('data.txt','a') as file:
+        file.write(stre+"\n")
+       return redirect('/insert')
+  
+
+    
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+               email=request.form.get('email')
+               password=request.form.get('password')
+            
+               with open('data.txt','r') as file:
+                for i in file.readlines():
+                    er, pwd = i.strip().split(' ')
+                    if er == email and pwd == password:
+                        return render_template('welcome.html',user=email)
+               
+    else:   
+        
+        return render_template('welcome.html',er)     
+               
+            
+
+                    
+
+
+
+
+
+
 
 
 if __name__=='__main__':    
